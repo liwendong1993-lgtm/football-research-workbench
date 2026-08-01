866333c1a7ba8cf29f81cecd21b8a79bd2adf1e3
liwendong1993-lgtm <liwendong1993@gmail.com>
2026-07-31 21:34:38 -0700

    Test 001-016 scan sequence ordering

--- tests/scan-utils.test.js
+++ tests/scan-utils.test.js
@@ -1,6 +1,19 @@
 const test=require('node:test');
 const assert=require('node:assert/strict');
-const {formatScanRow}=require('../scan-utils.js');
+const {formatScanRow,sortMatchesBySequence}=require('../scan-utils.js');
+
+test('扫盘图按比赛序号而不是开赛时间排列',()=>{
+  const matches=[
+    ...Array.from({length:9},(_,index)=>({num:`周六${String(index+8).padStart(3,'0')}`,time:index<2?'00:00':'08:00'})),
+    ...Array.from({length:7},(_,index)=>({num:`周六${String(index+1).padStart(3,'0')}`,time:`${String(18+index).padStart(2,'0')}:00`})),
+    {num:'自定义',time:'17:00'}
+  ];
+  assert.deepEqual(
+    sortMatchesBySequence(matches).map(match=>match.num),
+    [...Array.from({length:16},(_,index)=>`周六${String(index+1).padStart(3,'0')}`),'自定义']
+  );
+  assert.equal(matches[0].num,'周六008');
+});
 
 test('扫盘行把多选胜平负、让球、进球数和比分格式化为紧凑文本',()=>{
   const match={num:'周五201',time:'01:00',league:'瑞超',home:'哥德堡',away:'布鲁马波',hhad:{goalLine:'-1'}};
